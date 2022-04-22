5d10382a63302c04ac73021c2df4970f48756147
Anickris <UtteRus@mail.ru>
2022-04-22 22:32:52 +0500

    создание\запись файлов

--- main.py
+++ main.py
@@ -12,16 +12,15 @@ def get_forecast(lat: float = 57.09, lon: float = 65.32,
     return requests.get(URL_BASE, params=locals()).json()
 
 
-pogoda = get_forecast()
+
 
 
 def get_pogoda(pogoda):
     daily_pogoda = pogoda.get('daily')
     day_forecast = {}
     for day in daily_pogoda:
-        day_time = datetime.fromtimestamp(day['dt'])
-
-        day_forecast[f'{day_time}'] = [
+        day_time = str(datetime.fromtimestamp(day['dt']))
+        day_forecast[f'{day_time[:10]}'] = [
             f'Погода утром {int(day["temp"]["morn"])}',
             f'Погода днём {int(day["temp"]["day"])}',
             f'Погода вечером {int(day["temp"]["eve"])}',
@@ -32,7 +31,25 @@ def get_pogoda(pogoda):
             f'Небо {day["weather"][0]["description"]}',
         ]
 
-    print(day_forecast)
+    return day_forecast
 
 
-get_pogoda(pogoda)
+
+
+
+def set_file_forecast(forestcast):
+    for element in forestcast.keys():
+        file_name = Path(f'day_forestcast/{element}.txt')
+        file_name.touch()
+
+
+def set_info_in_file_forecast(pogoda):
+    for txt_file in Path('day_forestcast').glob("*.txt"):
+        if txt_file.with_suffix('').name in pogoda.keys():
+            stroka = ' '.join(pogoda.get(txt_file.with_suffix('').name))
+            txt_file.write_text(stroka)
+
+
+set_file_forecast(get_pogoda(get_forecast()))
+pogoda = get_pogoda(get_forecast())
+set_info_in_file_forecast(pogoda)
